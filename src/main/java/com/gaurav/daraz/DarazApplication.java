7624f44cd699a1c705e26c278684f9898bb3cef5
Gaurav-kushwaha20 <gaurav.kushwaha.0825@gmail.com>
2025-10-13 17:51:41 +0545

    project setup

---
+++ src/main/java/com/gaurav/daraz/DarazApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.daraz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarazApplication.class, args);
+	}
+
+}
